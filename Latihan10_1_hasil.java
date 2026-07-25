b1ef5653eb2a4ea0d074722cb406a00c3d39faae
Reza Luna 2501091005 <rezaluna2313@gmail.com>
2026-07-25 17:06:37 +0700

    Update Latihan10_1_hasil.java

--- Latihan10_1_hasil.java
+++ Latihan10_1_hasil.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Adellia070426;
+package rza070426;
 
 /**
  *
